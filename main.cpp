c03e9626955122eb9ac105ad660bb6abe14be7a2
Kevin Shi <xpspectre@gmail.com>
2018-02-16 21:54:55 -0500

    Start dist mat fun that takes in templated samples and dist fun The approach compiles and works, but CLion incorrectly complains about the dist function pointer when called.

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ double euclidean(const vector<double>& x1, const vector<double>& x2) {
     return sqrt(d2);
 }
 
-
 double levenstein(const string& s1, const string& s2, double del, double ins, double sub) {
     // Edit distance of strings using user-specified costs
+    // Note: doesn't use default params since function pointers passed as args into other functions don't recognize them
     // Source: http://www.python-course.eu/levenshtein_distance.php
 
     // For all i and j, dist[i,j] will contain the Levenshtein distance between the first i characters of a and the
@@ -68,8 +68,20 @@ double levenstein(const string& s1, const string& s2) {
     return levenstein(s1, s2, del, ins, sub);
 }
 
+template <class T>
+vector<double> dist_mat(const vector<T>& x, double (*dist)(const T&, const T&)) {
+    // Distance matrix between all pairs in x, in order of x, in vector form (lower triangle entries), using specified
+    // distance function
+    auto nx = x.size();
+    vector<double> d (nx * (nx-1) / 2, 0.0);
+    d[0] = dist(x[0], x[1]);
+    // TODO: and so on
+    return d;
+}
+
 int main() {
     // Test out Euclidean distance of 2 vectors of doubles
+    cout << "Calculating Euclidean distances..." << endl;
     vector<double> x1 {0.0, 0.0};
     vector<double> x2 {1.1, 2.2};
 
@@ -78,18 +90,19 @@ int main() {
     cout << d << endl;
 
     // Test out edit (Levenstein) distance of 2 strings
+    cout << "Calculating edit distances..." << endl;
     string s1 = "abc";
     string s2 = "bcd";
     string s3 = "";  // empty
     string s4 = "abcd";
-
-//    cout << s1 << " " << s2 << endl;
 
     cout << levenstein(s1, s2) << endl;
     cout << levenstein(s1, s1) << endl;
     cout << levenstein(s1, s3) << endl;
     cout << levenstein(s1, s4) << endl;
 
+    // Some more strings
+    cout << "Calculating more edit distances..." << endl;
     string t1 = "g5a23g5a3av34acra2ct";
     string t2 = "bacbzV#35v#V#nk";
     string t3 = "fdafdsafsafsa";
@@ -97,6 +110,14 @@ int main() {
     cout << levenstein(t1, t2) << endl;  // 17
     cout << levenstein(t2, t3) << endl;  // 15
     cout << levenstein(t1, t2, 0.9, 1.1, 2.0) << endl;
+
+    // Test out distance matrix calc
+    cout << "Calculating distance matrix..." << endl;
+    vector<string> strs {s1, s2, s3, s4};
+    vector<double> dm = dist_mat(strs, levenstein);  // CLion incorrectly says dist arg is an error
+    for (double dmi : dm) {
+        cout << dmi << endl;
+    }
 
     return 0;
 }
