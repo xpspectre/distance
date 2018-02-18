f22be08cd59931881b7419b8b3b39cf72f4862cc
Kevin Shi <xpspectre@gmail.com>
2018-02-17 22:31:46 -0500

    Link pthread required on Linux; cleanup example tests Works on Linux. Link pthread works in the cygwin env on Windows, but should probably make it env specific. The catch2 linking is much faster on Linux (almost instant, and actually everything runs faster), which makes unit test dev easy.

--- test_distance.cpp
+++ test_distance.cpp
@@ -4,18 +4,6 @@
 using std::vector;
 using std::string;
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-//    REQUIRE( Factorial(0) == 1 );
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
-
 TEST_CASE( "Euclidean distance", "[euclidean_dist]" ) {
     vector<double> x1 {0.0, 0.0};
     vector<double> x2 {1.0, 0.0};
